eb9d5466878f14484eafafd41856e89e98be73ae
Darshita <darshitakochar2000@gmail.com>
2023-06-28 10:16:59 +0530

    change1-production

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ app.use('/api/users/',userRoute)
 app.use('/api/orders/',ordersRoute)
 
 
-if(process.env.NODE_ENV === 'production')
-{
-    app.use('/' , express.static('client/build'))
+    app.use( express.static(path.join(__dirname,'./client/build')));
 
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname , '/client/build/index.html'))
-    })
-}
+        res.sendFile(path.join(__dirname , '/client/build/index.html'));
+    });
+
 /*app.get("/",(req,res)=>{
     res.send("Server working");
 }); */
